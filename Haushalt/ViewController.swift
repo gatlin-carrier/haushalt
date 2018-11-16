9fab23fd777e12e079e34adb10481b6ef933bde7
Gatlin Carrier <gatlin.carrier@gmail.com>
2018-11-16 13:30:43 -0500

    Initial Commit

---
+++ Haushalt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Haushalt
+//
+//  Created by Gatlin Carrier on 11/16/18.
+//  Copyright © 2018 Gatlin Carrier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
